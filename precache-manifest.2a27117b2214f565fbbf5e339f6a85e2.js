984d32250e8cce164300bf058ede66a402ca498e
Mike Jackson <jacksonm@ilpeaus.local>
2019-10-28 22:10:44 -0400

    Updates

---
+++ precache-manifest.2a27117b2214f565fbbf5e339f6a85e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c12490e3816d4161040d4c7c955f4f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baa6c2d50e055fb02187",
+    "url": "/static/css/main.303fa83c.chunk.css"
+  },
+  {
+    "revision": "0f5cac06eb71eeb39f98",
+    "url": "/static/js/2.ab836829.chunk.js"
+  },
+  {
+    "revision": "baa6c2d50e055fb02187",
+    "url": "/static/js/main.5eb03615.chunk.js"
+  },
+  {
+    "revision": "c1cdcb9d4958f317006b",
+    "url": "/static/js/runtime-main.dc30b89c.js"
+  }
+]);
